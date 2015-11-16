e889674495ef2f4625f15533f1e16637e724d4d4
a15alejandroog <a15alejandroog@iessanclemente.net>
2015-11-16 09:15:04 +0100

    Commit

---
+++ app/src/main/java/com/example/a15alejandroog/ud_a1a_a15alejandroog/create_person.java
@@ -0,0 +1,13 @@
+package com.example.a15alejandroog.ud_a1a_a15alejandroog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class create_person extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_person);
+    }
+}

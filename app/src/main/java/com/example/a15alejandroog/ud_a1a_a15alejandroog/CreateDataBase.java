5cf7f6fcc28b9cb29fd812b6b89c5221442fbe53
a15alejandroog <a15alejandroog@iessanclemente.net>
2015-11-25 11:40:14 +0100

    Commit 25/11/2015

--- app/src/main/java/com/example/a15alejandroog/ud_a1a_a15alejandroog/CreateDataBase.java
+++ app/src/main/java/com/example/a15alejandroog/ud_a1a_a15alejandroog/CreateDataBase.java
@@ -16,6 +16,7 @@ public class CreateDataBase extends SQLiteOpenHelper {
     public SQLiteDatabase database;
     public final static String DB_NAME = "DATOS";
     public final static int DB_VERSION = 1;
+    private static String SELECT_PERSOA = "SELECT * FROM persoa WHERE nome = ?";
     private String CT_PERSOA = "CREATE TABLE persoa(" +
             "nome VARCHAR(50) PRIMARY KEY," +
             "descricion VARCHAR(150))";
@@ -25,16 +26,51 @@ public class CreateDataBase extends SQLiteOpenHelper {
         // TODO Auto-generated constructor stub
     }
 
+    /**
+     * Add a person
+     * @param person an object from the person class to add
+     */
     public void addPerson(Persoa person) {
         ContentValues values = new ContentValues();
         values.put("nome", person.getNome());
         values.put("descricion", person.getDescricion());
+        database = getWritableDatabase(); //Open database connection (write mode)
         database.insert("persoa", null, values);
+        database.close(); //Close connection
     }
 
+    /**
+     * List a person from the database
+     * @param nome the name of the person
+     * @return an object from the person class
+     */
+    public Persoa listPerson(String nome){
+        Persoa persoa = null;
+        database = getReadableDatabase();//Open database connection (read mode)
+        Cursor datosConsulta = database.rawQuery(SELECT_PERSOA, new String[] {String.valueOf(nome)});
+        database.close();//Close connection
+
+        //Create object
+        if (datosConsulta.moveToFirst()) {
+            while (!datosConsulta.isAfterLast()) {
+                persoa = new Persoa(datosConsulta.getString(0),datosConsulta.getString(1));
+                datosConsulta.moveToNext();
+            }
+        }
+        return persoa;
+    }
+
+    /**
+     * List all the people from the database
+     * @return a list of Persoa objects
+     */
     public ArrayList<Persoa> listPeople() {
         ArrayList<Persoa> listPeople = new ArrayList<>();
+        database = getReadableDatabase();//Open database connection (read mode)
         Cursor consult = database.rawQuery("SELECT * FROM persoa", null);
+        database.close();//Close connection
+
+        //Create object and add them to the ArrayList
         if (consult.moveToFirst()) {
             Persoa person;
             while (!consult.isAfterLast()) {
@@ -45,6 +81,7 @@ public class CreateDataBase extends SQLiteOpenHelper {
         }
         return listPeople;
     }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CT_PERSOA);
@@ -52,6 +89,6 @@ public class CreateDataBase extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        // TODO Auto-generated method stub
+        //TODO Auto-generated method stub
     }
 }
--- app/src/main/java/com/example/a15alejandroog/ud_a1a_a15alejandroog/UD_A1A_a15alejandroog.java
+++ app/src/main/java/com/example/a15alejandroog/ud_a1a_a15alejandroog/UD_A1A_a15alejandroog.java
@@ -1,5 +1,7 @@
 package com.example.a15alejandroog.ud_a1a_a15alejandroog;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,10 +11,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.File;
+
 public class UD_A1A_a15alejandroog extends AppCompatActivity {
 
     private CreateDataBase dataBase;
+    AlertDialog.Builder alert;
     Button btn_show;
+    String dataBasePath;
     Button btn_create;
 
     @Override
@@ -20,6 +26,29 @@ public class UD_A1A_a15alejandroog extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ud__a1_a_a15alejandroog);
 
+        dataBasePath = "/data/data/" + getPackageName() + "/databases/" + CreateDataBase.DB_NAME;
+        File dBase = new File(dataBasePath);
+
+        if (!dBase.exists()) {
+            alert = new AlertDialog.Builder(this);
+            alert.setIcon(android.R.drawable.ic_dialog_alert);
+            alert.setTitle((CharSequence) getString(R.string.dialog_title));
+            alert.setItems(R.array.choices, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    switch (which) {
+                        case 0:
+                            dataBase = new CreateDataBase(getApplicationContext());
+                            break;
+                        case 1:
+                            
+                            break;
+                    }
+                }
+            });
+            alert.show();
+
+        }
         dataBase = new CreateDataBase(getApplicationContext());
         Log.i("DB", "Created data base");
 
@@ -41,6 +70,10 @@ public class UD_A1A_a15alejandroog extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void copyDB(){
+
     }
 
     @Override
